ecb64d31499395904a3da9636929b1b1a1603148
izzy-Ti <israelashenafi29@gmail.com>
2025-08-29 14:26:54 -0700

    Random and players

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,5 +4,24 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Lottery {
 
+    address public manager;
+    address payable[] public players;
+    address payable public winner;
 
+    constructor(){
+        manager = msg.sender;
+    }
+
+    function participate() public payable{
+        require(msg.value == 1 ether , 'Please you must pay one ether');
+        players.push(payable(msg.sender));
+    }
+
+    function totalBalance() public view returns (uint){
+        require(msg.sender == manager, 'Sorry you must be an Admin to see');
+        return address(this).balance;
+    }
+    function random() public view returns (uint){
+        return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players.length)));
+    }
 }
