f904f538cff64ef104e075f1f7f70ec2840b0bdd
izzy-Ti <israelashenafi29@gmail.com>
2025-08-29 14:54:37 -0700

    _Full_contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -13,15 +13,24 @@ contract Lottery {
     }
 
     function participate() public payable{
-        require(msg.value == 1 ether , 'Please you must pay one ether');
+        require(msg.value == 1 ether , "Please you must pay one ether");
         players.push(payable(msg.sender));
     }
 
     function totalBalance() public view returns (uint){
-        require(msg.sender == manager, 'Sorry you must be an Admin to see');
+        require(msg.sender == manager, "Sorry you must be an Admin to see");
         return address(this).balance;
     }
     function random() public view returns (uint){
         return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players.length)));
+    }
+    function pickWinner() public{
+        require(manager == msg.sender, "sorry you must be an admin");
+        require(players.length >= 3, "The players must be grater than 3");
+        uint r= random();
+        uint index = r% players.length;
+        winner = players[index];
+        winner.transfer(totalBalance());
+        players = new address payable[](0);
     }
 }
